4a1b324cbe895b4125befc359741a59a57b186a3
shiki-01 <0110shiki@gmail.com>
2024-06-27 15:39:42 +0900

    save

--- src/routes/project/[name]/+page.svelte
+++ src/routes/project/[name]/+page.svelte
@@ -1,25 +1,31 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { projectTab, activeProject } from '$lib/store';
+	import List from '$lib/List.svelte';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import { onDestroy, onMount } from 'svelte';
-
-	onMount(async () => {
-		const parm = $page.params.name;
-		activeProject.set(parm);
-		unsubscribe();
-	});
+	import { X, House } from 'lucide-svelte';
+	import { Button } from '$lib/components/ui/button';
 
 	let tabs: string[] = [];
+	let intervalId: ReturnType<typeof setInterval>;
 
-	async function unsubscribe() {
+	async function updateTabs() {
 		if (typeof window !== 'undefined') {
 			const data = await window.electron.system.getTabs();
 			tabs = data.map((tab: { name: any }) => tab.name);
 		}
 	}
 
+	onMount(async () => {
+		const parm = $page.params.name;
+		activeProject.set(parm);
+		await updateTabs();
+		intervalId = setInterval(updateTabs, 5000);
+	});
+
 	onDestroy(() => {
-		unsubscribe();
+		if (intervalId) clearInterval(intervalId);
 	});
 
 	function removeTabs(tab: string) {
@@ -27,23 +33,38 @@
 			window.electron.system.removeTabs(tab);
 		}
 		tabs = tabs.filter((t) => t !== tab);
-	}
-
-	$: {
-		if (tabs.length >= 0 && typeof window !== 'undefined') {
-			window.location.href = `/`;
+		if (tabs.length === 0) {
+			window.location.href = '/';
 		}
 	}
 </script>
 
 <main class="">
 	{#if tabs.length > 0}
-		{#each tabs as tab}
-			<div>
-				{tab}
-				<button on:click={() => removeTabs(tab)}>Close</button>
+		<div class="flex flex-rows gap-4 px-4 border-y-2 align-middle">
+			<div class="flex justify-end items-center align-middle">
+				<Dialog.Root>
+					<Dialog.Trigger>
+						<Button variant="ghost" class="hover:bg-slate-50 h-5 p-0">
+							<House size={15} />
+						</Button>
+					</Dialog.Trigger>
+					<Dialog.Content class="max-w-[500px] max-h-[500px] overflow-y-auto">
+						<div>
+							<List />
+						</div>
+					</Dialog.Content>
+				</Dialog.Root>
 			</div>
-		{/each}
+			{#each tabs as tab}
+				<div class="flex justify-between items-center">
+					<Button variant="ghost" class="hover:bg-slate-50 h-5">{tab}</Button>
+					<Button variant="ghost" class="w-5 h-5 p-0" on:click={() => removeTabs(tab)}>
+						<X size={15} />
+					</Button>
+				</div>
+			{/each}
+		</div>
 	{:else}
 		<div>No tabs</div>
 		<a href="/">Go back</a>
